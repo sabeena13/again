d6c2464e175b3667b43131ce2a48c83926ae4b7e
Sabina Gasimova <54109365+sabeena13@users.noreply.github.com>
2019-09-22 23:59:22 +0400

    pifaqor

---
+++ pifaqor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{  int a,b,c;
+cin>>a>>b>>c;
+if (a*a+b*b==c*c)
+{
+	cout<<"pifaqor ededleridir";
+}
+else {
+	cout<<"pifaqor ededleri deyiler";
+}
+
+return 0;}
