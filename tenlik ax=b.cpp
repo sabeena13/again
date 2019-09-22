d19d565b7d60260126421a6f7d8019d6772ce73c
Sabina Gasimova <54109365+sabeena13@users.noreply.github.com>
2019-09-22 23:51:26 +0400

    tenlik ax=b

---
+++ tenlik ax=b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int a,b,x;
+	cin>>a>>b;
+	if (a!=0 && b!=0) //yegane  hell
+	{
+		x=b/a;
+    	cout<<"yegane  helli var"<<":"<<x;	}
+if (a==0 && b!=0) //hell yoxdur
+{  cout<<"helli yoxdur";}
+if (a==0 && b==0) //sonsuz sayda helli var
+{	cout<<"sonsuz sayda helli var";
+}
+ return 0;
+
+} 
