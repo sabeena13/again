f4c968d91d271f4d06cd0cc2173abdd4fdac7c51
Sabina Gasimova <54109365+sabeena13@users.noreply.github.com>
2019-09-23 01:07:09 +0400

    fibonacci

---
+++ fibonacci.cpp
@@ -0,0 +1,17 @@
+#include  <iostream>
+using namespace std;
+int main ()
+{
+int  n,m,l;
+cin>>n>>m>>l;
+if (l==n+m)
+{
+	cout<<"l fibonacci ededidir";
+}
+else
+{ 
+
+	cout<<"l fibonacci ededi deyil";
+}
+return 0;
+}
